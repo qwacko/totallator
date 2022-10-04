7ed63acab7f4fb4ba59770115e675cf37be95372
James <james@jmcdowall.com>
2022-10-05 12:19:52 +1300

    Added Mermaid To Docsaurus

--- docs/docs/accountGroupings/account-grouping-basics.md
+++ docs/docs/accountGroupings/account-grouping-basics.md
@@ -5,3 +5,11 @@ sidebar_position: 1
 # Basics
 
 Bla Bla Bla
+
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+```
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -4,124 +4,129 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: "Totallator",
-  tagline: "Totallator Documentation",
-  url: "https://docs.totallator.com",
-  baseUrl: "/totallator/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+async function createConfig() {
+  const mdxMermaid = await import("mdx-mermaid");
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  /** @type {import('@docusaurus/types').Config} */
+  return {
+    title: "Totallator",
+    tagline: "Totallator Documentation",
+    url: "https://docs.totallator.com",
+    baseUrl: "/totallator/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "facebook", // Usually your GitHub org/user name.
+    projectName: "docusaurus", // Usually your repo name.
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+      defaultLocale: "en",
+      locales: ["en"],
+    },
+
+    presets: [
+      [
+        "classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            remarkPlugins: [mdxMermaid.default],
+            // Please change this to your repo.
+            // Remove this to remove the "edit this page" links.
+            editUrl:
+              "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          },
+          blog: {
+            showReadingTime: true,
+            // Please change this to your repo.
+            // Remove this to remove the "edit this page" links.
+            editUrl:
+              "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        navbar: {
+          title: "My Site",
+          logo: {
+            alt: "My Site Logo",
+            src: "img/logo.svg",
+          },
+          items: [
+            {
+              type: "doc",
+              docId: "intro",
+              position: "left",
+              label: "Docs",
+            },
+            {
+              href: "https://github.com/qwacko/totallator",
+              label: "GitHub",
+              position: "right",
+            },
+          ],
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "Docs",
+              items: [
+                {
+                  label: "Tutorial",
+                  to: "/docs/intro",
+                },
+              ],
+            },
+            // {
+            //   title: "Community",
+            //   items: [
+            //     {
+            //       label: "Stack Overflow",
+            //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            //     },
+            //     {
+            //       label: "Discord",
+            //       href: "https://discordapp.com/invite/docusaurus",
+            //     },
+            //     {
+            //       label: "Twitter",
+            //       href: "https://twitter.com/docusaurus",
+            //     },
+            //   ],
+            // },
+            {
+              title: "More",
+              items: [
+                {
+                  label: "GitHub",
+                  href: "https://github.com/facebook/docusaurus",
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} Totallator. Built with Docusaurus.`,
         },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
         },
       }),
-    ],
-  ],
+  };
+}
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: "My Site",
-        logo: {
-          alt: "My Site Logo",
-          src: "img/logo.svg",
-        },
-        items: [
-          {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Docs",
-          },
-          {
-            href: "https://github.com/qwacko/totallator",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          // {
-          //   title: "Community",
-          //   items: [
-          //     {
-          //       label: "Stack Overflow",
-          //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
-          //     },
-          //     {
-          //       label: "Discord",
-          //       href: "https://discordapp.com/invite/docusaurus",
-          //     },
-          //     {
-          //       label: "Twitter",
-          //       href: "https://twitter.com/docusaurus",
-          //     },
-          //   ],
-          // },
-          {
-            title: "More",
-            items: [
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Totallator. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-};
-
-module.exports = config;
+module.exports = createConfig;
