a53b173659eb309dc97e4d3c704fe980e79fcb19
James <james@jmcdowall.com>
2022-10-04 14:12:18 +1300

    FIxed Cleanse Object

--- src/lib/utils/cleanseObject.ts
+++ src/lib/utils/cleanseObject.ts
@@ -1,4 +1,4 @@
-import { chain, isEmpty, isUndefined, omitBy } from 'lodash-es';
+import { chain, isEmpty, isUndefined, mapValues, omitBy } from 'lodash-es';
 
 export const cleanseObject = <
 	InType extends Record<string, unknown>,
@@ -7,39 +7,22 @@ export const cleanseObject = <
 	input: InType | undefined
 ): OutType | undefined => {
 	if (input) {
-		console.log({ cleanseObject: true, input });
-		console.log({ chain: chain(input).value() });
-		console.log({ firstOmit: chain(input).omitBy(isUndefined).value() });
-		console.log({
-			mapped: chain(input)
-				.omitBy(isUndefined)
-				.mapValues((item) => {
-					if (typeof item === 'object') {
-						if (Array.isArray(item)) {
-							return item;
-						}
-						const output = cleanseObject<InType, OutType>(item as InType);
-						return output;
-					}
+		const withoutUndefined = omitBy(input, isUndefined);
+		const arraysMapped = mapValues(withoutUndefined, (item) => {
+			if (typeof item === 'object') {
+				if (Array.isArray(item)) {
 					return item;
-				})
-				.value()
-		});
-
-		return chain(input)
-			.omitBy(isUndefined)
-			.mapValues((item) => {
-				if (typeof item === 'object') {
-					if (Array.isArray(item)) {
-						return item;
-					}
-					const output = cleanseObject<InType, OutType>(item as InType);
-					return output;
 				}
-				return item;
-			})
-			.omitBy(isEmpty)
-			.value() as OutType;
+				const output = cleanseObject<InType, OutType>(item as InType);
+				return output;
+			}
+			return item;
+		});
+		const omitEmpty = omitBy(arraysMapped, isEmpty);
+
+		console.log({ input, omitEmpty });
+
+		return omitEmpty as OutType;
 	}
 	return undefined;
 };
