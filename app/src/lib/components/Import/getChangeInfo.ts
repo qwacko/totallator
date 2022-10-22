9748ce3a37494692d571dc6fc0a77a9f5320d3e8
James <james@jmcdowall.com>
2022-10-23 00:05:39 +1300

    Fixed Import Table Text

--- app/src/lib/components/Import/getChangeInfo.ts
+++ app/src/lib/components/Import/getChangeInfo.ts
@@ -4,18 +4,30 @@ export const getChangeInfo = (info?: ImportChecksReturn | null) => {
 	if (!info) {
 		return { text: '', colour: { orange: false, green: false } };
 	}
+
+	const newItem =
+		!info.dataChanged && !info.idInItemList && !info.idInJournals && !info.dataInJournals;
+	const updateExisting = info.dataChanged;
+
+	const preText = newItem
+		? 'New'
+		: updateExisting
+		? 'Update'
+		: info.idInItemList
+		? 'Existing Unchanged'
+		: undefined;
+
 	const infoText = [
-		info.idInItemList ? 'ID Found' : undefined,
+		preText,
 		info.idInJournals ? 'ID In Journal List' : undefined,
-		info.dataInJournals ? 'Title In Journal List' : undefined,
-		info.dataChanged ? 'Data Changed' : undefined
+		info.dataInJournals ? 'Title In Journal List' : undefined
 	]
 		.filter((item) => item)
 		.join(',');
 
 	const colourOutput = {
 		orange: Boolean(info.dataChanged),
-		green: !info.dataChanged && !info.idInItemList && !info.idInJournals && !info.dataInJournals
+		green: newItem
 	};
 
 	return { text: infoText, colour: colourOutput };
