2ad5adbc0cbe4aa4e1366d5f104733857b8eca48
qwacko <james@jmcdowall.com>
2023-02-03 19:10:13 +0000

    First Graph Created

--- src/components/account/AccountStats.tsx
+++ src/components/account/AccountStats.tsx
@@ -1,9 +1,22 @@
+import { Group } from "@mantine/core";
+import { Bar, ComposedChart, Line, Tooltip } from "recharts";
+
 import { useAccountStats } from "src/utils/hooks/accounts/useAccountStats";
 
 import { StatsDisplay } from "../reusable/StatsDisplay";
 
 export const AccountStats = ({ id }: { id: string }) => {
-  const { data: stats } = useAccountStats({ id });
+  const { data: stats, historicalData } = useAccountStats({ id });
 
-  return <StatsDisplay stats={stats} />;
+  return (
+    <Group>
+      <StatsDisplay stats={stats} />
+      <ComposedChart data={historicalData} width={200} height={60}>
+        <Line type="monotone" dataKey="total" stroke="#8884d8" dot={false} />
+        <Bar dataKey="positive" fill="#8884d8" />
+        <Bar dataKey="negative" fill="#8884d8" />
+        <Tooltip />
+      </ComposedChart>
+    </Group>
+  );
 };
--- src/utils/hooks/accounts/useAccountStats.tsx
+++ src/utils/hooks/accounts/useAccountStats.tsx
@@ -1,9 +1,6 @@
-import { eachMonthOfInterval, format, set, subDays, subMonths } from "date-fns";
-import { useEffect, useMemo } from "react";
+import { useMemo } from "react";
 import { z } from "zod";
 
-import { trpc } from "src/utils/trpc";
-import type { JournalFilterValidationInputType } from "src/utils/validation/journalEntries/getJournalValidation";
 import type { SummaryInputValidationType } from "src/utils/validation/summary/summaryInputValidation";
 import {
   summaryReturnCoreValidation,
@@ -11,6 +8,7 @@ import {
 } from "src/utils/validation/summary/summaryReturnValidation";
 
 import { useParsedSummary } from "../summary/useParsedSummary";
+import { useHistoricalData } from "../summary/useHistoricalData";
 
 const accountReturnValidation = z.array(
   summaryReturnCoreValidation.merge(
@@ -22,76 +20,6 @@ const accountIdGroupingList: SummaryInputValidationType["groupingList"] = [
   "accountId"
 ];
 const defaultValue: z.infer<typeof accountReturnValidation> = [];
-
-const useHistoricalData = (
-  filters: JournalFilterValidationInputType[],
-  months = 12
-) => {
-  const [startDate, prevDate] = useMemo(() => {
-    const targetDate = set(subMonths(new Date(), months), { date: 0 });
-    return [targetDate, subDays(targetDate, 1)];
-  }, [months]);
-
-  const { data: startingAmount } = trpc.summary.getTimeData.useQuery({
-    filters: [...filters, { date: { lte: prevDate } }],
-    groupingList: ["accountGroupingId"]
-  });
-
-  const { data: positiveAmounts } = trpc.summary.getTimeData.useQuery({
-    filters: [...filters, { date: { gte: startDate }, amount: { gte: 0 } }],
-    groupingList: ["yearMonth"]
-  });
-
-  const { data: negativeAmounts } = trpc.summary.getTimeData.useQuery({
-    filters: [...filters, { date: { gte: startDate }, amount: { lte: 0 } }],
-    groupingList: ["yearMonth"]
-  });
-
-  const returnData = useMemo(() => {
-    const endDate = new Date();
-    const dateIntervals = eachMonthOfInterval({
-      start: startDate,
-      end: endDate
-    });
-    const groupedData = dateIntervals.map((item) => {
-      const key = format(item, "yyyy-MM");
-      const pos = positiveAmounts
-        ? positiveAmounts.find((item) => item.yearMonth === key)
-        : undefined;
-      const neg = negativeAmounts
-        ? negativeAmounts.find((item) => item.yearMonth === key)
-        : undefined;
-      // console.log(key, { pos });
-      return {
-        key,
-        positive: pos?._sum?.amount ? pos._sum.amount : 0,
-        negative: neg?._sum?.amount ? neg._sum.amount : 0
-      };
-    });
-
-    const startTotal =
-      startingAmount && startingAmount[0] && startingAmount[0]._sum?.amount
-        ? startingAmount[0]._sum.amount
-        : 0;
-
-    const groupedWithTotals = groupedData.map((item, index) => {
-      const posTotal = groupedData
-        .slice(0, index)
-        .reduce((prev, current) => prev + current.positive, 0);
-      const negTotal = groupedData
-        .slice(0, index)
-        .reduce((prev, current) => prev + current.negative, 0);
-
-      const total = posTotal + negTotal + startTotal;
-
-      return { ...item, total };
-    });
-
-    return groupedWithTotals;
-  }, [startDate, startingAmount, positiveAmounts, negativeAmounts]);
-
-  return returnData;
-};
 
 export const useAccountStats = ({ id }: { id: string }) => {
   const memoFilters = useMemo(() => [{ accountId: { in: [id] } }], [id]);
