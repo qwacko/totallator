de3ce91234cd13589f918ecdf5c36a1154bda9f0
qwacko <james@jmcdowall.com>
2023-05-22 22:12:21 +0000

    superjson time.

--- src/lib/server/trpc/t.ts
+++ src/lib/server/trpc/t.ts
@@ -1,4 +1,8 @@
 import type { Context } from './context';
 import { initTRPC } from '@trpc/server';
 
-export const t = initTRPC.context<Context>().create();
+import superjson from 'superjson';
+
+export const t = initTRPC.context<Context>().create({
+	transformer: superjson
+});
